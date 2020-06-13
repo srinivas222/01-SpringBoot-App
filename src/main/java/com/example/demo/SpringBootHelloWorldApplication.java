631b75637ab2a34f2ebb43e4bc89782073977c4b
Anil-Doppalapudi <anil.doppalapudi@savvysoft.net>
2020-06-13 22:50:03 +0530

    k variable added

--- src/main/java/com/example/demo/SpringBootHelloWorldApplication.java
+++ src/main/java/com/example/demo/SpringBootHelloWorldApplication.java
@@ -9,6 +9,7 @@ import org.springframework.context.annotation.ComponentScan;
 public class SpringBootHelloWorldApplication {
 		int j=20;
 	int i=10;
+	int k=30;
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootHelloWorldApplication.class, args);
 	}
