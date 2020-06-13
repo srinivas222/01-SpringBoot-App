15a6c059032a2c1a244c140b7dc0c95cc7e95226
Anil-Doppalapudi <anil.doppalapudi@savvysoft.net>
2020-06-13 22:35:01 +0530

    one variable added

--- src/main/java/com/example/demo/SpringBootHelloWorldApplication.java
+++ src/main/java/com/example/demo/SpringBootHelloWorldApplication.java
@@ -7,7 +7,7 @@ import org.springframework.context.annotation.ComponentScan;
 @SpringBootApplication
 @ComponentScan(basePackages = "com.example.controller")
 public class SpringBootHelloWorldApplication {
-
+		int j=20;
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootHelloWorldApplication.class, args);
 	}
