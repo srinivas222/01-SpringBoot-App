898de670504153ee1ae68d9c84a8dddd356b6a70
Anil-Doppalapudi <anil.doppalapudi@savvysoft.net>
2020-06-13 23:11:10 +0530

    one method added

--- src/main/java/com/example/demo/SpringBootHelloWorldApplication.java
+++ src/main/java/com/example/demo/SpringBootHelloWorldApplication.java
@@ -17,5 +17,8 @@ public class SpringBootHelloWorldApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootHelloWorldApplication.class, args);
 	}
+	public void done(){
+		
+	}
 
 }
